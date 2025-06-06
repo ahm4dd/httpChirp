44ee9c91fe3c7bf4d76e4036397598625fa22a58
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-07 01:43:20 +0300

    Added a way to log requests with not-ok status codes

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middlewares.js";
 const app = express();
 const PORT = 8080;
 app.use("/app", express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
+app.use(middlewareLogResponses);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middlewares.js";
 
 const app = express();
 const PORT = 8080;
@@ -8,6 +9,7 @@ const PORT = 8080;
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
+app.use(middlewareLogResponses);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
