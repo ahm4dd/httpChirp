07f7395945bd3642fbe66e011382b8221a4766a5
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-08 00:42:03 +0300

    Updated validateChirp to check for profanity

--- dist/api/validateChirp.js
+++ dist/api/validateChirp.js
@@ -5,7 +5,20 @@ export async function handlerValidateChirp(req, res) {
         res.end();
     }
     else {
-        res.status(200).send(JSON.stringify({ valid: true }));
+        params.body = params.body.trim();
+        let profane = ["kerfuffle", "sharbert", "fornax"];
+        let filteredWords = [];
+        for (const word of params.body.split(" ")) {
+            if (profane.includes(word.toLowerCase())) {
+                let asterisks = "*".repeat(4);
+                filteredWords.push(asterisks);
+            }
+            else
+                filteredWords.push(word);
+        }
+        res
+            .status(200)
+            .send(JSON.stringify({ cleanedBody: filteredWords.join(" ") }));
         res.end();
     }
 }
--- src/api/validateChirp.ts
+++ src/api/validateChirp.ts
@@ -1,6 +1,4 @@
 import { Response, Request } from "express";
-import express from "express";
-import { parse } from "path";
 export async function handlerValidateChirp(req: Request, res: Response) {
   type parameters = {
     body: string;
@@ -10,7 +8,18 @@ export async function handlerValidateChirp(req: Request, res: Response) {
     res.status(400).send(JSON.stringify({ error: "Chirp is too long" }));
     res.end();
   } else {
-    res.status(200).send(JSON.stringify({ valid: true }));
+    params.body = params.body.trim();
+    let profane: string[] = ["kerfuffle", "sharbert", "fornax"];
+    let filteredWords: string[] = [];
+    for (const word of params.body.split(" ")) {
+      if (profane.includes(word.toLowerCase())) {
+        let asterisks = "*".repeat(4);
+        filteredWords.push(asterisks);
+      } else filteredWords.push(word);
+    }
+    res
+      .status(200)
+      .send(JSON.stringify({ cleanedBody: filteredWords.join(" ") }));
     res.end();
   }
 }
