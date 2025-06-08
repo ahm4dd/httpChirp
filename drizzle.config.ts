867c1a47de419e43271ee3056b22f917b51fb145
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-08 06:26:29 +0300

    Added users queries and connected the database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
-
+process.loadEnvFile("./credientials.env");
 export default defineConfig({
   schema: "src/db/schema.ts",
   out: "src/db/drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: "",
+    url: process.env.DB_URL!,
   },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
+import process from "process";
+process.loadEnvFile("./../credientials.env");
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
-export const config: APIConfig = { fileserverHits: 0 };
+export const config: APIConfig = {
+  fileserverHits: 0,
+  dbURL: process.env.DB_URL!,
+};
