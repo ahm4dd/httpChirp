175856eb7763178d99129008f5e7ae520ae0be49
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-09 03:26:28 +0300

    Added automatic migration code

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,22 @@
+import { MigrationConfig } from "drizzle-orm/migrator";
+
 import process from "process";
 process.loadEnvFile();
 type APIConfig = {
   fileserverHits: number;
+  db: DBConfig;
+};
+
+export type DBConfig = {
   dbURL: string;
+  migrationConfig: MigrationConfig;
+};
+
+const migrationConfig: MigrationConfig = {
+  migrationsFolder: "src/db/drizzle/",
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
-  dbURL: process.env.DB_URL!,
+  db: { dbURL: process.env.DB_URL!, migrationConfig: migrationConfig },
 };
